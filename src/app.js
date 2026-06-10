dda243e02392a4abad52110fc87a47b3db45eeb7
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-06-10 19:31:08 +0300

    start

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import { getIce } from "./appi/getIce";
+
+
+const listEl = document.querySelector(".list");
+
+
+getIce().then(res => createIceMurcup(res));
+
+
+function createIceMurcup(arr) {
+    const item = arr.map(({id, image, calories, description, name, price, type}) => {
+        return `<li class="item" id="${id}">
+            <img src="${image}" alt="${description}" class="img">
+            <p>${name}</p>
+            <p>${price}</p>
+            <p>${type}</p>
+            <p>${description}</p>
+            <p>${calories}</p>
+        </li>`
+    }).join("")
+
+    listEl.innerHTML = item
+
+}
+
+
